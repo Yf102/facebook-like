f89ab21c5d86609a88988c2ade2d6f1fafdf0847
Filip Hristov <philipx192@gmail.com>
2023-12-03 20:27:49 +0200

    Fixed import

--- tests/handlers/index.ts
+++ tests/handlers/index.ts
@@ -1,11 +1,11 @@
 import { rest } from "msw";
-import { Posts } from "@/tests/__mocks__/data/Posts";
+import { postsData } from "@/tests/__mocks__/data/Posts";
 
 export const handlers = [
   rest.options("http://localhost:3000/api/*", (_req, res, ctx) => {
     return res(ctx.json({}));
   }),
   rest.get("http://localhost:3000/api/posts", (_req, res, ctx) => {
-    return res(ctx.json(Posts), ctx.delay(50));
+    return res(ctx.json(postsData), ctx.delay(50));
   }),
 ];
